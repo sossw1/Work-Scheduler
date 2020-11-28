ade75af4c70a4f9f471469b0d07fc7d7b5d2d37f
sossw1 <sossw1@gmail.com>
2020-11-28 17:27:34 -0500

    Created time blocks

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // Date at top
 $("#currentDay").html(moment().format('dddd[, ]MMMM Do'));
 
+// Create time blocks
+var hours = [9,10,11,12,1,2,3,4,5];
+for(let i=0; i<hours.length; i++) {
+    var row = `<div class='row row${i}'>`;
+    var hour = `<div class='hour col-md-1'>${hours[i]}`;
+    var text = `<textarea class='col-md-10'>`;
+    var save = `<button class="saveBtn col-md-1">`;
+    $(".container").append(row);
+    var currentRow = $(`.row${i}`)
+    currentRow.append(hour);
+    currentRow.append(text);
+    currentRow.append(save);
+
+};
