3504046940b9c7f871577d14c49ef91f4f2c27ba
sossw1 <sossw1@gmail.com>
2020-11-29 10:21:10 -0500

    Responsive xs and sm

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ $("#currentDay").html(moment().format('dddd[, ]MMMM Do'));
 var hours = [9,10,11,12,1,2,3,4,5];
 for(let i=0; i<hours.length; i++) {
     var row = `<div class='row row${i}'>`;
-    var hour = `<div class='hour col-md-1'>${hours[i]}`;
-    var text = `<textarea class='col-md-10'>`;
-    var save = `<button class="saveBtn col-md-1">`;
+    var hour = `<div class='hour col-2 col-sm-2 col-md-1'>${hours[i]}`;
+    var text = `<textarea class='col-8 col-sm-8 col-md-10'>`;
+    var save = `<button class="saveBtn col-2 col-sm-2 col-md-1">`;
     $(".container").append(row);
     var currentRow = $(`.row${i}`)
     currentRow.append(hour);
