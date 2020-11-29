aa9c95f763cf43d96af5e535b03e76225c1efa27
sossw1 <sossw1@gmail.com>
2020-11-29 10:59:28 -0500

    Find block index of current hour

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ var hours = [9,10,11,12,1,2,3,4,5];
 for(let i=0; i<hours.length; i++) {
     var row = `<div class='row row${i}'>`;
     var hour = `<div class='hour col-2 col-sm-2 col-md-1'>${hours[i]}`;
-    var text = `<textarea class='col-8 col-sm-8 col-md-10'>`;
+    var text = `<textarea class='textarea${i}'>`;
     var save = `<button class="saveBtn col-2 col-sm-2 col-md-1">`;
     $(".container").append(row);
     var currentRow = $(`.row${i}`)
     currentRow.append(hour);
     currentRow.append(text);
     currentRow.append(save);
-
 };
+// Find out which time block INDEX is present (if any)
+var present;
+var currentHour = parseInt(moment().format("hh"));
+for(let i=0; i<hours.length; i++) {
+    if(currentHour == hours[i]) {
+        present = i;
+    }
+}
